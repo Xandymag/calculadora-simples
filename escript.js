8d1749c819408bb01565cb2eacbda30725edf6a0
Xandytec <carvalhoxandy22@gmail.com>
2026-08-14 11:02:27 -0400

    botao limpar

--- escript.js
+++ escript.js
@@ -13,4 +13,6 @@ form.addEventListener('submit', function(event) {
     } else if (operation === 'divide') {
         result = num2 !== 0 ? num1 / num2 : 'Erro: Divisão por zero';
     }resultDiv.textContent = `Resultado: ${result}`;
-});
+});function limparResultado() {
+    document.getElementById("display").value = "";
+}
